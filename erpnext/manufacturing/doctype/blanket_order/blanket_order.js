brian-pond/erpnext

commit 652076c8164e8aced3007621e0083bf48e44e429

Due Date, Delivery Note Date, Blankets

erpnext/manufacturing/doctype/blanket_order/blanket_order.js
@@ -5,6 +5,7 @@
 frappe.provide("erpnext.manufacturing");
 
 frappe.ui.form.on('Blanket Order', {
+
 	onload: function(frm, cdt, cdn) {
 
 		cur_frm.dashboard.frm.fields[0].df.hidden=1  // Hide the Dashboard
@@ -144,13 +145,38 @@ frappe.ui.form.on('Blanket Order', {
 });
 
 
-// Brian: Still not quite sure why I'm wrapping/extending form.Controller
-// but it's necessary to call get_item_details() successfully
+/* 
+	Brian: Still not quite sure why I'm wrapping/extending form.Controller?
+	But it's necessary to call get_item_details() successfully
+*/
+	
 erpnext.manufacturing.BlanketOrder = frappe.ui.form.Controller.extend({
 
 	refresh: function() {
 		var me = this;  // because the meaning of 'this' changes in contexts
 		erpnext.hide_company();
+
+		this.frm.add_custom_button(__('Recalculate Quantity on PO'), function() {
+			me.frm.call({
+				method: "update_ordered_qty",
+				doc: me.frm.doc,
+				callback: function() {
+					frappe.msgprint(`Finished recalculating PO Qty Ordered, per blanket order line.`);
+					console.log('Finished recalculating PO quantities.');
+				}
+			});
+		});
+
+		this.frm.add_custom_button(__('View PO Line Allocations'), function() {
+			me.frm.call({
+				method: "view_allocated_po_lines",
+				doc: me.frm.doc,
+				callback: function() {
+					frappe.msgprint(`Finished recalculating PO Qty Ordered, per blanket order line.`);
+					console.log('Finished recalculating PO quantities.');
+				}
+			});
+		}, "View");
 
 		frappe.run_serially([
 			() => {		// First action
@@ -161,7 +187,7 @@ erpnext.manufacturing.BlanketOrder = frappe.ui.form.Controller.extend({
 				if (this.frm.doc.supplier && single_blanket_per_po) {
 					this.frm.add_custom_button(__('View Purchase Orders'), function() {
 						frappe.set_route('List', 'Purchase Order', {blanket_order: me.frm.doc.name});
-					});
+					}, "View");
 				}
 				else if (this.frm.doc.supplier && this.frm.doc.docstatus === 1) {
 					this.frm.add_custom_button(__(`<div style="text-decoration: line-through;">
@@ -209,6 +235,7 @@ erpnext.manufacturing.BlanketOrder = frappe.ui.form.Controller.extend({
 
 $.extend(cur_frm.cscript, new erpnext.manufacturing.BlanketOrder({frm: cur_frm}));
 
+
 frappe.ui.form.on("Blanket Order Item", {
 	/*
 		* Yes, the line's controller functions are defined here; Child Tables are a 2nd class citizen in Frappe
@@ -253,12 +280,29 @@ frappe.ui.form.on("Blanket Order Item", {
 		else {
 			blanket_line.qty_in_weight_uom = blanket_line.qty * blanket_line.weight_per_unit;
 		}
-		refresh_field("items");  // Yes, we have to refresh in the context of the Form.
 		recalc_line_amount(cdt, cdn);
 		recalculate_total_weight(cdt, cdn);
+		refresh_field("items");  // Yes, we have to refresh in the context of the Form.
 
 		// For now, I don't have a Stock Quantity field.
 		// frappe.model.set_value(cdt, cdn, 'stock_qty', blanket_line.qty * blanket_line.conversion_factor);
+	},
+
+	qty_in_weight_uom: function(doc, cdt, cdn) {
+		// When 'qty_in_weight_uom' changes, update 'qty'.  Then trigger standard code from there forward.
+		// console.log("DEBUG: entered method Blanket Order Line method 'qty_in_weight_uom'");
+		let row = frappe.get_doc(cdt, cdn);
+		// 1. Update qty
+		if (row.uom_buying === row.uom_weight) {
+			row.qty = row.qty_in_weight_uom;
+			console.log("Making qty = qty_in_weight_uom")
+		}
+		else {
+			row.qty = row.qty_in_weight_uom / row.weight_per_unit;
+		}
+		recalc_line_amount(cdt, cdn);
+		recalculate_total_weight(cdt, cdn);
+		refresh_field("items");	
 	},
 
 	reqd_by_date: function(frm, cdt, cdn) {
@@ -303,33 +347,41 @@ frappe.ui.form.on("Blanket Order Item", {
 	},
 
 	rate: (frm, cdt, cdn) => {
-		let row = frappe.get_doc(cdt, cdn);
-		// frappe.model.set_value(cdt, cdn, 'stock_qty', row.qty * row.conversion_factor);
+		let order_line = frappe.get_doc(cdt, cdn);
+
+		// Scenario 1.  Both UOM are the same:
+		if (order_line.uom_weight === order_line.uom_buying) {
+			order_line.rate_per_weight_uom = order_line.rate;
+		}
+		else {
+			// Example:  $5.00/lb = $300/drum divided by 60 lbs/drum.
+			order_line.rate_per_weight_uom = (order_line.rate / order_line.weight_per_unit);
+		}
 		recalc_line_amount(cdt, cdn);
+		refresh_field("items");  // Yes, we have to refresh in the context of the Form.
 	},
+
+	rate_per_weight_uom: (frm, cdt, cdn) => {
+		let order_line = frappe.get_doc(cdt, cdn);
+
+		// Scenario 1.  Both UOM are the same:
+		if (order_line.uom_weight === order_line.uom_buying) {
+			order_line.rate = order_line.rate_per_weight_uom;
+		}
+		else {
+			// Example:  $5.00/lb = $300/drum divided by 60 lbs/drum.
+			order_line.rate = (order_line.rate_per_weight_uom * order_line.weight_per_unit);
+		}
+		recalc_line_amount(cdt, cdn);
+		refresh_field("items");  // Yes, we have to refresh in the context of the Form.
+	},
+
 
 	conversion_factor: (frm, cdt, cdn) => {
 		let row = frappe.get_doc(cdt, cdn);
 		// frappe.model.set_value(cdt, cdn, 'stock_qty', row.qty * row.conversion_factor);
 	},
 
-	qty_in_weight_uom: function(doc, cdt, cdn) {
-		// When 'qty_in_weight_uom' changes, update 'qty'.  Then trigger standard code from there forward.
-		// console.log("DEBUG: entered method Blanket Order Line method 'qty_in_weight_uom'");
-		let row = frappe.get_doc(cdt, cdn);
-		// 1. Update qty
-		if (row.uom_buying === row.uom_weight) {
-			row.qty = row.qty_in_weight_uom;
-			console.log("Making qty = qty_in_weight_uom")
-		}
-		else {
-			row.qty = row.qty_in_weight_uom / row.weight_per_unit;
-			console.log("Making qty a calculation.")
-		}
-		recalc_line_amount(cdt, cdn);
-		recalculate_total_weight(cdt, cdn);
-		refresh_field("items");	
-	}
 });
 
 

erpnext/manufacturing/doctype/blanket_order/blanket_order.py
@@ -32,17 +32,15 @@ class BlanketOrder(Document):
 			order_line.line_amount = order_line.qty * order_line.rate
 			self.amount_grand_total += order_line.line_amount if order_line.line_amount else 0.0
 
-	# Spectrum Fruits: No reason to include a -range- of Dates for blanket orders.
-	# def validate_dates(self):
-	#	if getdate(self.from_date) > getdate(self.to_date):
-	#		frappe.throw(_("From date cannot be greater than To date"))
-
 	def update_ordered_qty(self):
 		"""
 		Spectrum Fruits: Loop through blanket lines, and update their 'ordered_qty' from POs.
 		"""
 		for line in self.items:
 			line.update_ordered_qty()
+			# frappe.msgprint(f"Blanket Order Line {line.idx} : Quantity on Submitted POs = {ret}")
+		frappe.msgprint("Finished updating PO Qty for all Blanket Order lines.")
+		self.reload()
 
 	def set_supplier_address(self):
 		address_dict = {
@@ -82,6 +80,15 @@ class BlanketOrder(Document):
 		# Order Lines:
 		frappe.db.sql("""UPDATE `tabBlanket Order Item` SET docstatus = 0 WHERE parent = %s""", self.name)
 
+	def view_allocated_po_lines(self):
+		html_message = []
+		for blanket_line in self.items:
+			row = f"Blanket Line {blanket_line.idx}: {blanket_line.item_code} ---- Quantity {blanket_line.qty}"
+			po_lines = frappe.get_list("Purchase Order Item", fields=["parent", "qty"], filters={"blanket_order_item": blanket_line.name})
+			for purchase_line in po_lines:
+				row += f"PO {purchase_line.parent}, Line {purchase_line.idx}, Quantity {purchase_line.qty}"
+		html_message.append(row)
+		frappe.msgprint(html_message, as_table=True)
 
 
 @frappe.whitelist()

erpnext/accounts/doctype/purchase_invoice/purchase_invoice.js
@@ -137,6 +137,10 @@ erpnext.accounts.PurchaseInvoice = erpnext.buying.BuyingController.extend({
 		}
 	},
 
+	// Spectrum Fruits.
+	due_date: function() {
+	},
+
 	unblock_invoice: function() {
 		const me = this;
 		frappe.call({

erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -52,6 +52,26 @@ class PurchaseInvoice(BuyingController):
 		supplier_tds = frappe.db.get_value("Supplier", self.supplier, "tax_withholding_category")
 		self.set_onload("supplier_tds", supplier_tds)
 
+	def before_validate(self):
+		"""
+		Execute code prior to 'validate()' Controller Method.
+		"""
+
+		# Spectrum Fruits: Try to clear the Payment Schedule table, if the Due Date is manually altered.
+		doc_orig = self.get_doc_before_save()
+
+		if doc_orig and (doc_orig.due_date != self.due_date):
+			# Due Date was altered.
+			# doc_payment_term = self.get_doc("Payment Term")
+			self.payment_schedule = []   # clear the table, then Rebuild it.
+			self.payment_terms_template = None
+			self.append('payment_schedule', {'payment_term': 'Term_Override',
+			                                 'description': 'Due date chosen manually.',
+										     'due_date': self.due_date,
+										     'invoice_portion': 100,
+										     'payment_amount': self.outstanding_amount
+			})
+
 	def before_save(self):
 		if not self.on_hold:
 			self.release_date = ''

erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -149,6 +149,7 @@ class SalesInvoice(SellingController):
 						frappe.throw(_("Row #{0}: Asset {1} cannot be submitted, it is already {2}").format(d.idx, d.asset, asset.status))
 
 	def before_save(self):
+		self.delivery_note_date = self.get_firstonly_delivery_note_date()  # SPECTRUM FRUITS
 		set_account_for_mode_of_payment(self)
 
 	def on_submit(self):
@@ -1287,6 +1288,30 @@ class SalesInvoice(SellingController):
 
 		if update:
 			self.db_set('status', self.status, update_modified = update_modified)
+
+
+	def get_firstonly_delivery_note_date(self):
+		"""
+		Spectrum Fruits Modification:  They require a Delivery Note Date on Sales Invoices.
+
+		There is nothing in ERPNext that will prevent 2+ Delivery Notes to be referenced by a single Sales Invoice.
+		Therefore, this function chooses the most-recent date of all results.
+		"""
+		delivery_note_names = [ invoice_line.delivery_note for invoice_line in self.items ]
+		if len(delivery_note_names) == 0:
+			return None
+
+		fields = [ "posting_date" ]
+		filters = [
+			["name", "in", delivery_note_names ]
+		]
+		delivery_note_dates = frappe.get_all('Delivery Note', fields=fields, filters = filters)  # list of dict
+		delivery_note_dates = sorted([ each['posting_date'] for each in delivery_note_dates], reverse=True)  # list of dates
+
+		if not delivery_note_dates:
+			return None
+		return max(delivery_note_dates)
+
 
 def get_discounting_status(sales_invoice):
 	status = None

erpnext/accounts/party.py
@@ -365,12 +365,16 @@ def validate_due_date(posting_date, due_date, party_type, party, company=None, b
 			return
 
 		if default_due_date != posting_date and getdate(due_date) > getdate(default_due_date):
-			is_credit_controller = frappe.db.get_single_value("Accounts Settings", "credit_controller") in frappe.get_roles()
+			# Spectrum Fruits : Adding some extra text and validation here.
+			credit_controller_role = frappe.db.get_single_value("Accounts Settings", "credit_controller")
+			if not credit_controller_role:
+				msgprint(_("Warning: No 'Credit Manager' has been configured under 'Accounts Settings'."))
+			is_credit_controller = credit_controller_role in frappe.get_roles()
 			if is_credit_controller:
 				msgprint(_("Note: Due / Reference Date exceeds allowed customer credit days by {0} day(s)")
 					.format(date_diff(due_date, default_due_date)))
 			else:
-				frappe.throw(_("Due / Reference Date cannot be after {0}")
+				frappe.throw(_("Due / Reference Date cannot be after {0}\n(to override, contact a User who is a Credit Manager in Account Settings)")
 					.format(formatdate(default_due_date)))
 
 @frappe.whitelist()

erpnext/manufacturing/doctype/blanket_order_item/blanket_order_item.py
@@ -9,23 +9,25 @@ from frappe.model.document import Document
 # SF_MOD_0001 : Blanket Order Items
 class BlanketOrderItem(Document):
 	def update_ordered_qty(self):
-		""" Update line 'ordered_qty' based on actual PO/SO value.
-			TODO: Rather than storing in SQL, can we not cache this and use a 'display method'?
 		"""
-		# Sum quantity per Blanket Order line name.
+		Update line 'ordered_qty' by aggregating the Submitted quantity of related PO lines.
+		"""
+		# TODO: Long Term: Rather than storing in SQL, can we not cache this and use a 'display method'?
 		blanket_order_type = frappe.get_doc('Blanket Order', self.parent).blanket_order_type
 		if blanket_order_type == "Selling":
 			return
 
 		sql_query = """
-			SELECT IFNULL(SUM(purchase_line.qty),0) as qty
-			FROM `tabPurchase Order Item` AS purchase_line
-			WHERE purchase_line.blanket_order_item = '{0}'
-			AND purchase_line.docstatus = 1
-			
-		""".format(self.name)
+			SELECT IFNULL(SUM(PurchaseLine.qty),0) 		AS qty
+			FROM `tabPurchase Order Item`	AS PurchaseLine
+			INNER JOIN `tabPurchase Order`		AS PurchaseOrder
+			ON PurchaseOrder.name = PurchaseLine.parent
+			WHERE PurchaseLine.blanket_order_item = %(blanket_line_key)s
+			AND PurchaseOrder.docstatus = 1
+		"""
 
-		# AND purchase_line.status not in ('Closed', 'Stopped')
-		item_ordered_qty = frappe.db.sql(sql_query)
+		po_quantities = frappe.db.sql(sql_query, values={'blanket_line_key': self.name})
 		# Cannot use the ORM, because we're not allowed to update Cancelled documents.
-		frappe.db.set_value('Blanket Order Item', self.name, 'ordered_qty',  item_ordered_qty[0][0])
+		frappe.db.set_value('Blanket Order Item', self.name, 'ordered_qty',  po_quantities[0][0])
+		frappe.db.commit()
+		return po_quantities[0][0]

erpnext/public/js/controllers/transaction.js
@@ -807,6 +807,10 @@ erpnext.TransactionController = erpnext.taxes_and_totals.extend({
 		// due_date is to be changed, payment terms template and/or payment schedule must
 		// be removed as due_date is automatically changed based on payment terms
 		if (this.frm.doc.due_date && !this.frm.updating_party_details && !this.frm.doc.is_pos) {
+
+			// SPECTRUM FRUITS:  OVERRIDE
+			return;  // quite spamming the screen with "Please clear the..."
+
 			if (this.frm.doc.payment_terms_template ||
 				(this.frm.doc.payment_schedule && this.frm.doc.payment_schedule.length)) {
 				var message1 = "";

commit b10398ae70644928f1586c128cf348128bd1aeaf

Repair the 'View PO Line Allocations' button on Blanket Order
view_allocated_po_lines() has never produced a correct report.  It is not
dead code: blanket_order.js:170 puts it behind a "View PO Line Allocations"
custom button under the View group, so this is a user-facing feature that has
been broken since 652076c816.

Four separate defects:

1. html_message.append(row) sat outside both loops, so only the final blanket
   line ever appeared -- every earlier line was built into `row` and then
   discarded.

2. 'idx' was read from each Purchase Order Item but never requested in the
   get_list() field list.  frappe._dict returns None for a missing key rather
   than raising, so every PO line rendered as "Line None" instead of failing
   loudly.

3. msgprint(as_table=True) builds its HTML as

       for r in msg: for c in r: <td>c</td>

   so each row must be a list.  It was handed a list of *strings*, which
   iterates characters and emits one table cell per character.  Even with the
   loop fixed, the output would have been unreadable.

4. The JS callback printed "Finished recalculating PO Qty Ordered", copied
   from the Recalculate button directly above it.  The server method already
   displays the table, so the redundant client-side msgprint is removed rather
   than reworded.

Now emits a proper six-column table -- blanket line, item, blanket qty, PO,
PO line, PO qty -- with one row per allocation, and a dashed row for blanket
lines that have no Purchase Order lines yet, since "nothing allocated" is
itself the answer the button is usually asked for.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01Y73FSaQvJ6ACzE9A8TbBgM

erpnext/manufacturing/doctype/blanket_order/blanket_order.js
@@ -172,8 +172,8 @@ erpnext.manufacturing.BlanketOrder = frappe.ui.form.Controller.extend({
 				method: "view_allocated_po_lines",
 				doc: me.frm.doc,
 				callback: function() {
-					frappe.msgprint(`Finished recalculating PO Qty Ordered, per blanket order line.`);
-					console.log('Finished recalculating PO quantities.');
+					// The server method displays the allocation table itself.
+					console.log('Finished fetching PO line allocations.');
 				}
 			});
 		}, "View");

erpnext/manufacturing/doctype/blanket_order/blanket_order.py
@@ -79,14 +79,27 @@ class BlanketOrder(Document):
 		frappe.db.sql("""UPDATE `tabBlanket Order Item` SET docstatus = 0 WHERE parent = %s""", self.name)
 
 	def view_allocated_po_lines(self):
-		html_message = []
+		"""
+		Show which Purchase Order lines are allocated to each Blanket Order line.
+
+		msgprint(as_table=True) iterates the cells of each row, so every row must be a
+		list.  Handing it a list of strings renders one table cell per character.
+		"""
+		html_message = [["Blanket Line", "Item", "Blanket Qty", "Purchase Order", "PO Line", "PO Qty"]]
 		for blanket_line in self.items:
-			row = f"Blanket Line {blanket_line.idx}: {blanket_line.item_code} ---- Quantity {blanket_line.qty}"
-			po_lines = frappe.get_list("Purchase Order Item", fields=["parent", "qty"], filters={"blanket_order_item": blanket_line.name})
+			po_lines = frappe.get_list("Purchase Order Item",
+			                           fields=["parent", "idx", "qty"],
+			                           filters={"blanket_order_item": blanket_line.name},
+			                           order_by="parent, idx")
+			blanket_columns = [blanket_line.idx, blanket_line.item_code, blanket_line.qty]
+			if not po_lines:
+				html_message.append(blanket_columns + ["-", "-", "-"])
+				continue
 			for purchase_line in po_lines:
-				row += f"PO {purchase_line.parent}, Line {purchase_line.idx}, Quantity {purchase_line.qty}"
-		html_message.append(row)
-		frappe.msgprint(html_message, as_table=True)
+				html_message.append(blanket_columns +
+				                    [purchase_line.parent, purchase_line.idx, purchase_line.qty])
+
+		frappe.msgprint(html_message, title="PO Line Allocations", as_table=True)
 
 
 @frappe.whitelist()